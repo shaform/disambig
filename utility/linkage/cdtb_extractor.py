5e61767e5a6e597028fe4000806b550d2b158cc7
Shaform <shaform@gmail.com>
2015-05-03 18:09:18 +0800

    update argument detection

--- utility/linkage/cdtb_extractor.py
+++ utility/linkage/cdtb_extractor.py
@@ -3,6 +3,7 @@ Outputs a text file with all CDTB passages and a file with connetives.
 """
 import argparse
 import os
+import re
 import sys
 import xml.etree.ElementTree as etree
 
@@ -10,6 +11,7 @@ from collections import defaultdict
 from glob import glob
 
 Explicit = '\u663e\u5f0f\u5173\u7cfb'
+Rsym = re.compile(r'\W')
 
 
 def process_commands():
@@ -64,7 +66,7 @@ if __name__ == '__main__':
     mapping_stats = defaultdict(int)
     before_stats = defaultdict(int)
     end_stats = defaultdict(int)
-    after_stats = defaultdict(int)
+    in_range = out_range = 0
 
     with open(args.output, 'w') as f, open(args.connective, 'w') as connective_output:
         for path in glob(os.path.join(args.input, '*')):
@@ -94,9 +96,11 @@ if __name__ == '__main__':
                             fname, p.get('ID'), article))
                         detected_num_of_paragraph += 1
 
-                    if r.get('ConnectiveType') == Explicit:
-                        # -- extract connective -- #
+                    is_explicit = r.get('ConnectiveType') == Explicit
 
+                    # -- extract connective -- #
+
+                    if is_explicit:
                         indices = tuple(
                             r.get('ConnectivePosition').split('&&'))
 
@@ -134,42 +138,66 @@ if __name__ == '__main__':
                         elif roleloc == 'abnormal':
                             abnormal += 1
 
-                        # -- extract arguments -- #
+                    # -- extract arguments -- #
 
-                        sent_indices = [get_offsets(i)
-                                        for i in r.get('SentencePosition').split('|')]
-                        sents = r.get('Sentence').split('|')
+                    sent_indices = [get_offsets(i)
+                                    for i in r.get('SentencePosition').split('|')]
+                    sents = r.get('Sentence').split('|')
 
-                        # check sentence correctness
-                        incorrect = False
-                        if len(sent_indices) != len(sents):
-                            print('# sentence num not matched')
+                    # check sentence correctness
+                    incorrect = False
+                    if len(sent_indices) != len(sents):
+                        print('# sentence num not matched')
+                        print('{} P{} R{}'.format(
+                            fname,
+                            p.get('ID'),
+                            r.get('ID')
+                        ))
+                        incorrect = True
+
+                    for index, sent in zip(sent_indices, sents):
+                        x, y = index
+                        if article[x:y] != sent:
+                            print('# wrong sentence position')
+                            print('{} P{} R{}'.format(
+                                fname,
+                                p.get('ID'),
+                                r.get('ID')
+                            ))
+                            print('correct:', sent,
+                                  'extracted:', article[x:y])
                             incorrect = True
 
-                        for index, sent in zip(sent_indices, sents):
-                            x, y = index
-                            if article[x:y] != sent:
-                                print('# wrong sentence position')
-                                print('{} P{} R{}'.format(
-                                    fname,
-                                    p.get('ID'),
-                                    r.get('ID')
-                                ))
-                                print('correct:', sent,
-                                      'extracted:', article[x:y])
-                                incorrect = True
-
-                        # generate sent stats
+                    # generate sent stats
+                    if is_explicit:
                         argument_stats[len(sents)] += 1
                         connective_stats[len(conncts)] += 1
                         mapping_stats['{}-{}'.format(
                             len(conncts),
                             len(sents))] += 1
+                        if sent_indices[0][0] <= indices[0][0] and indices[-1][-1] <= sent_indices[-1][-1]:
+                            in_range += 1
+                        else:
+                            out_range += 1
 
-                        if not incorrect:
-                            for x, y in sent_indices:
-                                before_stats[extract(article, x - 1, x)] += 1
-                                end_stats[extract(article, y - 1, y)] += 1
+                    if not incorrect:
+                        for x, y in sent_indices:
+                            before = extract(article, x - 1, x)
+                            start = extract(article, x, x + 1)
+                            end = extract(article, y - 1, y)
+                            after = extract(article, y, y + 1)
+                            before_stats[before] += 1
+                            end_stats[end] += 1
+
+                            if Rsym.match(before) is None or (after != '[None]' and Rsym.match(end) is None):
+                                print('##not in', before, start)
+                                print('##not in', end, after)
+                                print(extract(article, x, y))
+                                print('{} P{} R{}'.format(
+                                    fname,
+                                    p.get('ID'),
+                                    r.get('ID')
+                                ))
 
                 if detected_num_of_paragraph > 1:
                     print('strange {} {} {}'.format(
@@ -183,7 +211,9 @@ if __name__ == '__main__':
     print_distribution(connective_stats)
     print('\nmapping stats:')
     print_distribution(mapping_stats)
-    print('\nbefore stats:')
-    print_distribution(before_stats)
-    print('\nend stats:')
-    print_distribution(end_stats)
+    # print('\nbefore stats:')
+    # print_distribution(before_stats)
+    # print('\nend stats:')
+    # print_distribution(end_stats)
+
+    print('in range: {}, out range: {}'.format(in_range, out_range))
