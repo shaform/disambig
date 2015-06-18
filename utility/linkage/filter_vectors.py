6faca7d8ace088734138b85fcc6a96d74616925b
Shaform <shaform@gmail.com>
2015-06-18 10:27:37 +0800

    add count for words not found in filter_vectors

--- utility/linkage/filter_vectors.py
+++ utility/linkage/filter_vectors.py
@@ -22,11 +22,16 @@ def main():
             for t in l.rstrip('\n').split('\t', 1)[1].split(' '):
                 tokens.add(t)
 
+    remain_count = len(tokens)
     with open(args.vectors, 'r') as vf, open(args.output, 'w') as of:
         for l in vf:
             l = l.rstrip()
             if l.split(' ', 1)[0] in tokens:
+                remain_count -= 1
                 of.write(l + '\n')
+                if remain_count == 0:
+                    break
+    print('{} tokens not found'.format(remain_count))
 
 if __name__ == '__main__':
     main()
